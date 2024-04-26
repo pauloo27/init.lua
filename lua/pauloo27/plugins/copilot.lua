799179796648c757c6d5acd1708077245c5f5ec8
Paulo <git@pauloo27.dev>
2024-04-26 08:56:06 -0300

    feat: improve copilot config
    - markdown, yaml and etc
    - keep suggestions quiet by default

--- lua/pauloo27/plugins/copilot.lua
+++ lua/pauloo27/plugins/copilot.lua
@@ -3,9 +3,21 @@ return {
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
+    event = "InsertEnter",
     opts = {
+      filetypes = {
+        yaml = true,
+        markdown = true,
+        gitcommit = true,
+        help = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
+      },
       suggestion = {
-        auto_trigger = true,
+        auto_trigger = false,
         keymap = {
           accept = '<C-j>',
         },
