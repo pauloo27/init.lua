fd238fa0902874811e6ae0711876cc8c91586a90
Paulo <git@pauloo27.dev>
2024-05-18 15:38:07 -0300

    ref: remove local "vim.o" variable

--- lua/pauloo27/core/prefs.lua
+++ lua/pauloo27/core/prefs.lua
@@ -1,31 +1,30 @@
--- simple preferences
-local o = vim.o
+-- simple preferences :)
 
 -- line numbers
-o.nu = true
-o.rnu = true
+vim.o.nu = true
+vim.o.rnu = true
 
 -- enable mouse
-o.mouse = 'a'
+vim.o.mouse = 'a'
 
 -- <Tab> size and behavior
-o.expandtab = true
-o.tabstop = 2
-o.softtabstop = 2
-o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
 
 -- more "natural" spliting behavior
-o.splitbelow = true
-o.splitright = true
+vim.o.splitbelow = true
+vim.o.splitright = true
 
 -- show line at column 80
-o.colorcolumn = '80'
+vim.o.colorcolumn = '80'
 
 -- allow for :find <file> to search in subdirectories
-o.path = o.path .. '**'
+vim.o.path = vim.o.path .. '**'
 -- ignore node_modules
-o.wildignore = o.wildignore .. '*/node_modules/*'
+vim.o.wildignore = vim.o.wildignore .. '*/node_modules/*'
 
 -- keep undo history in the disk so it persists between sessions
-o.undofile = true
-o.undodir = vim.fn.expand("~/.cache/undodir")
+vim.o.undofile = true
+vim.o.undodir = vim.fn.expand("~/.cache/undodir")
