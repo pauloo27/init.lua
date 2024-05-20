5d3d0f52bc745e96ea3cd4d5f8c0dc8d553fb52a
Paulo <git@pauloo27.dev>
2024-05-19 23:35:37 -0300

    rev: "feat: remove theme, let's try the nvim 0.10 one :)"
    This reverts commit e846e8368082aa75b2068af605407617a5c02028.

--- lua/pauloo27/plugins/appearence.lua
+++ lua/pauloo27/plugins/appearence.lua
@@ -1,11 +1,32 @@
 local lazy_event = require('pauloo27.plugins.loader').lazy_event
 local defer = require('pauloo27.plugins.loader').defer
 
-vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
-vim.cmd('hi NonText guibg=NONE ctermbg=NONE')
-vim.cmd('hi NormalNC guibg=NONE ctermbg=NONE')
+local apply_theme = function()
+  -- "visual warning" that you are using root
+  if os.getenv("USER") == 'root'
+  then
+    vim.cmd("colorscheme darkblue")
+  else
+    vim.cmd("colorscheme catppuccin")
+  end
+
+  -- transparent background
+  vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
+  vim.cmd('hi NonText guibg=NONE ctermbg=NONE')
+  vim.cmd('hi NormalNC guibg=NONE ctermbg=NONE')
+end
+
+defer(apply_theme)
 
 return {
+  -- catppuccin theme 😸
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = true,
+    priority = 1000,
+  },
+
   -- icons and stuff 🕸️
   { "nvim-tree/nvim-web-devicons", lazy = true },
 
