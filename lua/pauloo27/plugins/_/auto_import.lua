4527f5a7acee91f8353d24cef550a4f811179eb9
Paulo <git@pauloo27.dev>
2024-07-08 19:23:34 -0300

    feat: fallback auto import handler

--- lua/pauloo27/plugins/_/auto_import.lua
+++ lua/pauloo27/plugins/_/auto_import.lua
@@ -1,10 +1,34 @@
 return function()
-  -- TODO: support other languages?
-  vim.lsp.buf.code_action({
-    apply = true,
-    context = {
-      only = { "source.addMissingImports.ts" },
-      diagnostics = {},
-    },
-  })
+  local ft = vim.bo.filetype
+
+  local ts_auto_import = function()
+    vim.lsp.buf.code_action({
+      apply = true,
+      context = {
+        only = { "source.addMissingImports.ts" },
+        diagnostics = {},
+      },
+    })
+  end
+
+  local fallback_auto_import = function()
+    vim.lsp.buf.code_action({
+      apply = true,
+      context = {
+        only = { "source.organizeImports" },
+        diagnostics = {},
+      },
+    })
+  end
+
+  local auto_import_per_ft = {
+    ['typescript'] = ts_auto_import,
+  }
+
+  local handler = auto_import_per_ft[ft]
+  if handler ~= nil then
+    handler()
+  else
+    fallback_auto_import()
+  end
 end
