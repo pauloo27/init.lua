ae80e3f3768e05c1c1e9847c1fc28237e16aa3f7
Paulo <git@pauloo27.dev>
2024-07-11 19:05:48 -0300

    fix: use rustup for rust-analyzer

--- lua/pauloo27/langs/rust.lua
+++ lua/pauloo27/langs/rust.lua
@@ -5,6 +5,7 @@ return {
   load = function(on_attach)
     require("lspconfig").rust_analyzer.setup({
       on_attach = on_attach,
+      cmd = { "rustup", "run", "stable", "rust-analyzer" },
       settings = {
         ["rust-analyzer"] = {
           check = {
