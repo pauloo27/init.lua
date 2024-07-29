874ba70c599cb6c467406356b2d6f17c09cba9da
Paulo <git@pauloo27.dev>
2024-07-29 18:32:31 -0300

    feat: better svelte

--- lua/pauloo27/langs/svelte.lua
+++ lua/pauloo27/langs/svelte.lua
@@ -12,6 +12,16 @@ return {
     })
   end,
   load = function(on_attach)
-    require("lspconfig").svelte.setup({ on_attach = on_attach })
+    require("lspconfig").svelte.setup({
+      on_attach = function(client)
+        vim.api.nvim_create_autocmd("BufWritePost", {
+          pattern = { "*.js", "*.ts" },
+          callback = function(ctx)
+            client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+          end,
+        })
+        on_attach(client)
+      end,
+    })
   end,
 }
