c9e3d3a67a4bda60deebeec6e561df7c7a4b6bf8
Paulo <git@pauloo27.dev>
2024-08-20 19:08:13 -0300

    feat: improve what gof can handle

--- lua/pauloo27/misc/gf.lua
+++ lua/pauloo27/misc/gf.lua
@@ -1,26 +1,32 @@
 -- "Go tO File" under cursor in other window
 -- credits: https://github.com/ELLIOTTCABLE/vim-gfriend
-local gof = function(theremotion, heremotion)
-  heremotion = heremotion or "e"
+local gof = function()
   local cword = vim.fn.expand("<cWORD>")
+
+  -- trim [] around the word
+  cword = cword:gsub("^%[", ""):gsub("%]$", "")
+
   local st = string.find(cword, "\v\f+:%d+")
   local en = string.match(cword, "\v\f+:%d+")
-  if en then
+
+  if en and st then
     cword = string.sub(cword, st, en - 1)
   end
-  local bits = vim.split(cword, ":")
 
   local starting_window = vim.api.nvim_get_current_win()
+  local bits = vim.split(cword, ":")
 
   vim.cmd("wincmd p")
   if vim.api.nvim_get_current_win() == starting_window then
-    vim.cmd(theremotion .. " " .. bits[1])
+    vim.cmd("vsp " .. bits[1])
   else
-    vim.cmd(heremotion .. " " .. bits[1])
+    vim.cmd("e " .. bits[1])
   end
 
   if #bits >= 2 then
-    vim.cmd(bits[2])
+    -- handle lines "range" (eg somefile:20-27)
+    local line_num = vim.split(bits[2], "-")[1]
+    vim.cmd(line_num)
   end
 end
 
