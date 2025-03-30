5d046f362310d04a3ed572113533b74f5a580084
Paulo <git@pauloo27.dev>
2025-03-30 09:41:55 -0300

    feat: update and deno

--- lua/pauloo27/langs/js_and_ts.lua
+++ lua/pauloo27/langs/js_and_ts.lua
@@ -8,6 +8,16 @@ local has_file_in_root = function(name)
   return #file >= 1
 end
 
+local w_denols = function(lang)
+  return function()
+    if has_file_in_root("deno.json") or has_file_in_root("deno.jsonc") then
+      return require("formatter.filetypes." .. lang).denofmt()
+    end
+
+    return nil
+  end
+end
+
 local w_eslint_d = function(lang)
   return function()
     if
@@ -42,6 +52,7 @@ return {
   },
   load_format = function()
     local set_ft_config = require("pauloo27.plugins._.format").set_ft_config
+
     set_ft_config("javascript", {
       w_eslint_d("javascript"),
       w_prettierd("javascript"),
@@ -50,6 +61,7 @@ return {
     set_ft_config("typescript", {
       w_eslint_d("typescript"),
       w_prettierd("typescript"),
+      w_denols("typescript"),
     })
 
     -- eww jsx, i think i am going to ... 🤢
@@ -64,7 +76,36 @@ return {
     })
   end,
   load = function(on_attach)
-    require("lspconfig").ts_ls.setup({ on_attach = on_attach })
+    local lspconfig = require("lspconfig")
+    lspconfig.denols.setup({
+      root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+      init_options = {
+        lint = true,
+        suggest = {
+          imports = {
+            hosts = {
+              ["https://deno.land"] = true,
+            },
+          },
+        },
+      },
+      on_attach = on_attach,
+    })
+
+    lspconfig.ts_ls.setup({
+      on_attach = on_attach,
+      root_dir = function(filename, bufnr)
+        local denoRootDir =
+          lspconfig.util.root_pattern("deno.json", "deno.json")(filename)
+        if denoRootDir then
+          return nil
+        end
+
+        return lspconfig.util.root_pattern("package.json")(filename)
+      end,
+      single_file_support = false,
+    })
+
     require("lspconfig").tailwindcss.setup({ on_attach = on_attach })
   end,
 }
